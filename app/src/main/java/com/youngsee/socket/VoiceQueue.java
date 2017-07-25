098f452a1e2a578e05197a22e261fa04c80173eb
yanweidtc <yanweigeorge@gmail.com>
2017-07-25 16:53:34 +0800

    add Socket Server

---
+++ app/src/main/java/com/youngsee/socket/VoiceQueue.java
@@ -0,0 +1,12 @@
+package com.youngsee.socket;
+
+/**
+ * Created by Administrator on 2017/7/20.
+ */
+
+public class VoiceQueue {
+    public String msg = null;
+    public String count = null;
+    public String url = null;
+    public String session_id = null;
+}
